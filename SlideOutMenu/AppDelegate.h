b5bba97fc5e0332334d26c653189b7902d459df4
Syngmaster <max6361@mail.ru>
2017-07-18 19:04:08 +0100

    First commit

---
+++ SlideOutMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlideOutMenu
+//
+//  Created by Syngmaster on 18/07/2017.
+//  Copyright © 2017 Syngmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlideOutMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlideOutMenu
+//
+//  Created by Syngmaster on 18/07/2017.
+//  Copyright © 2017 Syngmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
